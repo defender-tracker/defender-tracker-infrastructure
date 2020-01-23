bc884bf2a582301f083dd7f48dc372c54868ce7c
Ben Freke <benjaminfreke@gmail.com>
2020-01-23 23:57:10 +0000

    Updated buildspecs

--- provider.tf
+++ provider.tf
@@ -1,6 +1,7 @@
 ## Specifies the Region your Terraform Provider will server
 provider "aws" {
   region = "eu-west-2"
+  assume_role_policy = "arn:aws:iam::707979055938:role/service-role/codebuild-DefenderTrackerDeploy-service-role"
 }
 ## Specifies the S3 Bucket and DynamoDB table used for the durable backend and state locking
 
