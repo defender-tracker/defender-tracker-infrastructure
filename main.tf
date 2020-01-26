0a430c2417f5aa8e23d21a1ce6ba6c92d221e83d
Ben Freke <benjaminfreke@gmail.com>
2020-01-26 16:41:09 +0000

    Pipeline bug update

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "dynamodb_table" {
   source = "./modules/dynamo_db"
-  naming_prefix = "duccy_tracker"
+  naming_prefix = "defender_tracker"
 }
 
 module "aws_sns_topic" {
   source = "./modules/sns_topic"
-  naming_prefix = "duccy_tracker"
+  naming_prefix = "defender_tracker"
 }
 
 module "aws_iot_core" {
   source = "./modules/aws_iot_core"
-  naming_prefix = "duccy_tracker"
+  naming_prefix = "defender_tracker"
   dynamodb_table_name = module.dynamodb_table.dynamodb_table_name
   mqtt_topic = "defender/gpsupdate"
   sns_role_arn = module.aws_sns_topic.sns_role_arn
@@ -21,5 +21,5 @@ module "receive_data_lambda_function" {
   source = "./modules/lambda_functions/receive_data_lambda"
   dynamodb_table = module.dynamodb_table.dynamodb_table_name
   sns_topic_arn = module.aws_sns_topic.sns_topic_arn
-  naming_prefix = "duccy_tracker"
+  naming_prefix = "defender_tracker"
 }
